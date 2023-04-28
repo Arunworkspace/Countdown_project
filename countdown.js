da768880202cfb7461dc744bdb314bf52a4dd23e
Arun yadav <arunrao015@gmail.com>
2023-04-28 13:38:00 +0530

    first commit

---
+++ countdown.js
@@ -0,0 +1,26 @@
+const endDate=prompt("Please enter date in this format (DD MM YY)")
+document.getElementById("end-date").innerText= endDate;
+const inputs = document.querySelectorAll("input")
+// const clock=()=>{
+// }
+function clock(){
+    const end=new Date(endDate)
+    const now=new Date()
+    const diff=((end-now)/1000);
+    if(diff<0) return;
+    // console.log(diff);
+
+    inputs[0].value=Math.floor(diff/3600/24)// convert into days
+    inputs[1].value=Math.floor((diff/3600)%24)// convert into hours
+    inputs[2].value=Math.floor((diff/60)%60)// convert into minutes
+    inputs[3].value=Math.floor(diff%60)// convert into seconds
+
+
+}
+clock()
+setInterval(
+    ()=>{
+        clock()
+    },
+    1000
+)
